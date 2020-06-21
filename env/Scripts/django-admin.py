62fd3a6167642bf3bcd90a5d9463082837b9216b
KetraKrato <ketra2541@gmail.com>
2020-06-21 14:30:40 +0700

    UpforAon

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ketrakrato\desktop\intern_project_backend-develop\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
